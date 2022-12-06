e12bfff81ab5b7a47d6d85443889c08e1fbfaeeb
Maya Wassercug <mwassercug40@stuy.edu>
2022-12-06 17:23:19 -0500

    solved part 1

--- Day6/Part1.java
+++ Day6/Part1.java
@@ -1,1 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.HashSet;
+public class Part1{
+  public static void main(String[] args){
+    try{
+      File file = new File("input.txt");
+      Scanner input = new Scanner(file);
+      String data = input.next();
 
+      HashSet<Character> letters = new HashSet<Character>();
+      for (int i = 0; i < data.length() - 4; i += 1){
+        for (int j = 0; j < 4; j++){
+          letters.add( data.charAt(i+j) );
+        }
+        if (letters.size() == 4){
+          System.out.println(i+4);
+          break;
+        }
+        letters.clear();
+      }
+
+      input.close();
+    }catch (FileNotFoundException e){
+      System.out.println(e);
+    }
+
+  }
+}
