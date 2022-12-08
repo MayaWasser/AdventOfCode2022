a8bfc2c2b7b8c76edc27c825dbb8f606d57c5c91
Maya Wassercug <mwassercug40@stuy.edu>
2022-12-07 22:27:56 -0500

    solved part 1

--- Day7/Part1.py
+++ Day7/Part1.py
@@ -1,1 +1,63 @@
+input = open("input.txt", "r");
+lines = input.readlines()
 
+def addDirectory(directories, path):
+    if not path in directories.keys():
+        directories[path] = []
+    return directories
+
+# def findSize(paths):
+#     directorySizes = {}
+#     for directory in paths.keys():
+#         size = 0
+#         for file in directory:
+#             if (file.isdigit()):
+#                 size += int(file)
+#             else:
+#                 directorySize = findSize( directories[file] )
+#                 size += directorySize
+#         directorySizes[directory] = size
+#     return directorySizes
+
+def findSize(paths, dir):
+    size = 0
+    for file in dir:
+        if (file.isdigit()):
+            size += int(file)
+        else:
+            size += findSize(paths, paths[file] )
+    return size
+
+def findSizes(paths):
+    directorySizes = {}
+    for directory in paths.keys():
+        directorySizes[directory] = findSize(paths, directories[directory] )
+    return directorySizes
+
+directories = {}
+currentPath = ""
+for line in lines:
+    parts = line.split()
+    if (line.startswith("$ cd")):
+        if (parts[-1] == "/"):
+            currentPath = "/"
+        elif (parts[-1] == ".."):
+            currentPath = "/".join( currentPath.split("/")[:-2] ) + "/"
+        else:
+            currentPath += parts[-1] + "/"
+        addDirectory(directories, currentPath)
+
+    if (parts[0].isdigit()):
+        directories[currentPath].append( parts[0] )
+
+    if (parts[0] == "dir"):
+        directories[currentPath].append( currentPath + parts[1] + "/")
+
+sum = 0
+directorySizes = findSizes(directories)
+for size in directorySizes.values():
+    if size <= 100000:
+        sum += size
+print(sum)
+
+input.close()
