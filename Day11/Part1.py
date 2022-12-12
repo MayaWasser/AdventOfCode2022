d797a6e1c123b8e5063a5335977d8c86d3517c78
Maya Wassercug <mwassercug40@stuy.edu>
2022-12-11 20:50:58 -0500

    solved part 1

--- Day11/Part1.py
+++ Day11/Part1.py
@@ -1,1 +1,59 @@
+def applyOperation(old, operation):
+    if operation[2:] == "old":
+        num = old
+    else:
+        num = int(operation[2:])
+    if operation[0] == "*":
+        return old * num
+    if operation[0] == "+":
+        return old + num
 
+input = open("input.txt", "r")
+lines = input.readlines();
+
+worryLevels = [ ]
+operations = [ ]
+tests = [ ]
+ifTrue = [ ]
+ifFalse = [ ]
+count = [ ]
+
+for lineNum in range(len(lines)):
+    line = lines[lineNum].strip()
+    if lineNum % 7 == 1:
+        items = line[16:].split(",")
+        worryLevels.append( [ int(item.strip()) for item in items] )
+        count.append(0)
+    if lineNum % 7 == 2:
+        operations.append( line[21:] )
+    if lineNum % 7 == 3:
+        tests.append( int(line[19:]) )
+    if lineNum % 7 == 4:
+        ifTrue.append( int(line[25:]) )
+    if lineNum % 7 == 5:
+        ifFalse.append( int(line[26:]) )
+
+def turn(monkey):
+    for item in worryLevels[monkey]:
+        count[monkey] += 1
+        item = applyOperation(item, operations[monkey]) // 3
+        if item % tests[monkey] == 0:
+            worryLevels[ ifTrue[monkey] ].append(item)
+        else:
+            worryLevels[ ifFalse[monkey] ].append(item)
+    worryLevels[monkey] = []
+
+def round():
+    for i in range( len(count) ):
+        turn(i)
+
+for i in range(20):
+    round()
+
+topMonkey = max(count)
+count.remove(topMonkey)
+secondMonkey = max(count)
+ans = topMonkey * secondMonkey
+print(ans)
+
+input.close()
